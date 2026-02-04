4bb9199aeebf1df832120210b4a841cca8d18b0e
Parth Parmar <parthparmar4105@gmail.com>
2026-02-04 10:54:52 +0530

    Update: Change default password hint to StudentTracker2026!

--- frontend/src/AdminLogin.jsx
+++ frontend/src/AdminLogin.jsx
@@ -75,7 +75,7 @@ export default function AdminLogin({ onLogin }) {
                             {loading ? "Authenticating..." : "Sign In"}
                         </button>
                         <div className="login-footer">
-                            <p>Default password: admin123</p>
+                            <p>Default password: StudentTracker2026!</p>
                         </div>
                     </form>
                 </div>
